45f18ea10dac9e0314dde6a7381f495bb4effa75
F T <furkan._t@hotmail.com>
2014-12-04 13:21:43 -0500

    Further optimizations. Removed the csv library, replaced it with direct BufferedWriter. Less idiomatic but a lot more efficient and faster.

--- src/util/blockchain_csv_sync.clj
+++ src/util/blockchain_csv_sync.clj
@@ -2,32 +2,12 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io])
   (:import [com.google.bitcoin.utils BlockFileLoader]
-           [com.google.bitcoin.core Block NetworkParameters Transaction TransactionInput TransactionOutput]
-           [com.google.bitcoin.params MainNetParams]
-           [clojurewerkz.neocons.rest Connection Neo4JEndpoint]))
+           [com.google.bitcoin.core Block Transaction TransactionInput TransactionOutput]
+           [com.google.bitcoin.params MainNetParams]))
 
-(defn- main-chain
-  []
+(defn- main-chain []
   (seq (BlockFileLoader. (MainNetParams.)
                          (BlockFileLoader/getReferenceClientBlockFileList))))
-
-(def headers {:blk-nodes ["hash" "date"]
-              :blk-rels ["prevhash" "hash"]
-              :tx-nodes ["hash" "outputs"]
-              :tx-rels ["hash" "blkhash" "inputs"]})
-
-(defn- blk-node
-  [^Block blk]
-  [(.getHashAsString blk) (.getTime (.getTime blk))])
-
-(defn- blk-rel
-  [^Block blk]
-  [(.toString (.getPrevBlockHash blk)) (.getHashAsString blk)])
-
-(defn- tx-node
-  [^Transaction tx]
-  [(.getHashAsString tx) (clojure.string/join "," (map (fn [ix ^TransactionOutput val] (str ix ":" (.getValue val)))
-                                                       (range) (.getOutputs tx)))])
 
 (defn- extract-info-tx-input
   [^TransactionInput in]
@@ -36,23 +16,22 @@
         index (.getIndex outpoint)]
     (when (not (.isCoinBase in)) (str index ":" hash))))
 
-(defn- tx-rel
-  [^Transaction tx blkhash]
-  [(.getHashAsString tx) blkhash (clojure.string/join "," (map extract-info-tx-input (.getInputs tx)))])
-
 (defn gen-csv!
   [& {:keys [:folder] :or {:folder ""}}]
-  (with-open [blk-nodes-file (io/writer (str folder "blk-nodes.csv"))
-              blk-rels-file (io/writer (str folder "blk-rels.csv"))
-
-              tx-nodes-file (io/writer (str folder "tx-nodes.csv"))
-              tx-rels-file (io/writer (str folder "tx-rels.csv"))]
-    (csv/write-csv blk-nodes-file [(headers :blk-nodes)])
-    (csv/write-csv blk-rels-file [(headers :blk-rels)])
-    (csv/write-csv tx-nodes-file [(headers :tx-nodes)])
-    (csv/write-csv tx-rels-file [(headers :tx-rels)])
-    (doseq [^Block blk (take 80000 (main-chain))]
-      (csv/write-csv blk-nodes-file [(blk-node blk)])
-      (csv/write-csv blk-rels-file [(blk-rel blk)])
-      (csv/write-csv tx-nodes-file (map tx-node (.getTransactions blk)))
-      (csv/write-csv tx-rels-file (map #(tx-rel %1 (.getHashAsString blk)) (.getTransactions blk))))))
+  (with-open [blks-file (io/writer (str folder "blks.csv"))
+              txs-file (io/writer (str folder "txs.csv"))]
+    (.write blks-file "hash,date,prevhash\n")
+    (.write txs-file "hash,outputs,blkhash,inputs\n")
+    (doseq [^Block blk (main-chain)]
+      (let [blkhash (.getHashAsString blk)]
+        (.write blks-file (str blkhash "," (.getTime (.getTime blk)) "," (.toString (.getPrevBlockHash blk)) "\n"))
+        (doseq [^Transaction tx (.getTransactions blk)]
+          (.write txs-file (.getHashAsString tx))
+          (.write txs-file ",\"")
+          (.write txs-file (clojure.string/join "," (map (fn [ix ^TransactionOutput val] (str ix ":" (.getValue val)))
+                                                         (range) (.getOutputs tx))))
+          (.write txs-file "\",")
+          (.write txs-file blkhash)
+          (.write txs-file ",\"")
+          (.write txs-file (clojure.string/join "," (map extract-info-tx-input (.getInputs tx))))
+          (.write txs-file "\"\n"))))))
